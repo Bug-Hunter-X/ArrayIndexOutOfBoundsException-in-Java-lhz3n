47bca816ac8fd9b6bd7e0abf9772c3c38c410069
BugHunter-X <askzenai@gmail.com>
2024-12-09 14:58:06 +0100

    Add BugSolution.java file

---
+++ BugSolution.java
@@ -0,0 +1,8 @@
+public class MyClass {
+    public static void main(String[] args) {
+        int[] arr = new int[5];
+        if (0 <= 2 && 2 < arr.length) { // Check the array index
+            System.out.println(arr[2]); // Access a valid element
+        }
+    }
+}
